dc1a70ca5a61aa8c4f965e66658dbe3d6cda3668
Pomidorka <030503ira@gmail.com>
2022-12-01 20:35:16 +0300

    004
    004

---
+++ lab10/TestProject2/TestProject2/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using TestProject2.Properties;
+
+namespace TestProject2
+{
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Navigate().GoToUrl("https://vokladki.by");
+            driver.Manage().Window.Maximize();
+            
+        
+        }
+        [Test]
+        public void Test1()
+        {
+          
+            PageObjectMenu pageObjectMenu = new PageObjectMenu(driver);
+            pageObjectMenu.dostavkaButtonClick();
+            pageObjectMenu.CatalogButtonClick();
+            pageObjectMenu.ContactsButtonClick();
+            pageObjectMenu.KonstructorButtonClick();
+            pageObjectMenu.SkidkiButtonClick();
+            pageObjectMenu.AboutBrandButtonClick();
+            pageObjectMenu.OPtButtonClick();
+            pageObjectMenu.FAQButtonClick();
+        }
+    }
+}
